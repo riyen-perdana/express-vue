8769c390dd89e9d630403763f960a7e447866f1c
riyen-perdana <riyenperdana@uin-suska.ac.id>
2024-11-20 14:04:59 +0700

    commit

---
+++ controllers/UserController.js
@@ -0,0 +1,111 @@
+//import prisma client
+const prisma = require("../prisma/client");
+
+//import ValidationResult from express-validator
+const { validationResult } = require("express-validator");
+
+//import bcrypt
+const bcrypt = require("bcryptjs");
+
+//function findUsers
+const findUsers = async (req, res) => {
+    try {
+
+        //get all users from database
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+            orderBy: {
+                id: "desc",
+            },
+        });
+
+        //send response
+        res.status(200).send({
+            success: true,
+            message: "Get all users successfully",
+            data: users,
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+const createUser = async (req, res) => {
+    // Periksa hasil validasi
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        // Jika ada error, kembalikan error ke pengguna
+        return res.status(422).json({
+            success: false,
+            message: "Validation error",
+            errors: errors.array(),
+        });
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    try {
+        //insert data
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+            },
+        });
+
+        //return response json
+        res.status(201).send({
+            success: true,
+            message: "User created successfully",
+            data: user,
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+const findUserById = async (req, res) => {
+    try {
+        //find user
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(req.params.id),
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+        });
+
+        //send response
+        res.status(200).send({
+            success: true,
+            message: `Get user successfully : ${req.params.id}`,
+            data: user,
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+module.exports = { findUsers, createUser, findUserById };
--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,27 @@ const registerController = require('../controllers/RegisterController');
 //import login controller
 const loginController = require('../controllers/LoginController');
 
+//import user controller
+const userController = require('../controllers/UserController');
+
 //import validate register and login
 const { validateRegister, validateLogin } = require('../utils/validators/auth');
+
+//import validate user
+const { validateUser } = require('../utils/validators/user');
+
+const verifyToken = require('../middlewares/auth');
 
 //define route for register
 router.post('/register', validateRegister, registerController.register);
 
 //define route for login
 router.post('/login', validateLogin, loginController.login);
+
+//define route for user
+router.get('/admin/users', verifyToken, userController.findUsers);
+router.post('/admin/users', verifyToken, validateUser, userController.createUser);
+router.get('/admin/users/:id', verifyToken, userController.findUserById);
 
 //export router
 module.exports = router
